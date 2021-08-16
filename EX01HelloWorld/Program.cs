e88362d30f57d05346c06af6ac22b6befaaac2bc
emil41m5 <emil41m5@CV-BB-5340>
2021-08-16 10:29:42 +0200

    Add project files.

---
+++ EX01HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EX01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string schoolName;
+            schoolName = "AspIT";
+
+            schoolName = "AspIT - Nordjylland";
+
+            string motto = "Fokuseret Talent";
+
+            Console.WriteLine($"{schoolName}, er en skole i Aalborg. Mottoet for skolen er: {motto}.");
+        }
+    }
+}
